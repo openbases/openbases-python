f19e588e5cfb4a550cd740b273a04edefc25418e
Vanessa Sochat <vsochat@stanford.edu>
2018-09-02 09:26:06 -0400

    make whedon pretty

--- src/conf.py
+++ src/conf.py
@@ -89,7 +89,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+#html_theme = 'alabaster'
 html_logo = "_static/img/robot.png"
 
 # Theme options are theme-specific and customize the look and feel of a
